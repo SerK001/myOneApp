4b922ec56065f9cb41ddaeb3721f36b1c5761e99
Serg264 <109822151+Serg264@users.noreply.github.com>
2022-11-21 17:41:25 +0300

    .gitignore

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const http = require('node:http');
 
+const PORT = 3000;
+
 const server = http.createServer((req, res) => {
     res.writeHead(200, { 'Content-Type': 'application/json' });
     res.end(JSON.stringify({
@@ -7,5 +9,6 @@ const server = http.createServer((req, res) => {
     }));
 });
 
-server.listen(3000);
+console.log(`server is running at htt[://localhost:${PORT}`)
+server.listen(PORT);
 
---
+++ circle.js
@@ -0,0 +1,12 @@
+class Circle {
+    area (r){
+       return  Math.PI * r * r;
+    }
+
+    circulference (r){
+      return  2 * Math.PI * r;
+    }
+}
+
+module.exports = Circle;
+
